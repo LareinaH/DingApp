60606cb17ca7a5190659dff72b947f28309b0224
lquterqtd <378203086@qq.com>
2018-08-05 13:24:35 +0800

    init

---
+++ src/main/java/com/admin/ac/dingApp/DingAppApplication.java
@@ -0,0 +1,12 @@
+package com.admin.ac.dingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingAppApplication.class, args);
+	}
+}
